b61d283037f4184f25d8599945a97a3812d4e262
isisisisisisithch <dallucus@gmail.com>
2022-04-17 16:03:55 -0400

    created communityApp by springboot

---
+++ src/main/java/ca/bytetube/communityApp/CommunityappApplication.java
@@ -0,0 +1,13 @@
+package ca.bytetube.communityApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/bytetube/communityApp/CommunityappApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bytetube.communityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
