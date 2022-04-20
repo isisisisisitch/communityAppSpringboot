2c2b693ba66191be8f9aeb73148c37d81e54c552
isisisisisisithch <dallucus@gmail.com>
2022-04-20 10:28:44 -0400

    1.migrated kaptcha to springboot project

--- src/main/java/ca/bytetube/communityApp/config/web/MvcConfiguration.java
+++ src/main/java/ca/bytetube/communityApp/config/web/MvcConfiguration.java
@@ -1,7 +1,10 @@
 package ca.bytetube.communityApp.config.web;
 
 
+import com.google.code.kaptcha.servlet.KaptchaServlet;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +13,8 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.servlet.ServletException;
 
 
 /**
@@ -75,6 +80,51 @@ public class MvcConfiguration implements WebMvcConfigurer, ApplicationContextAwa
         return multipartResolver;
     }
 
+
+    @Value("${kaptcha.border}")
+    private String border;
+
+    @Value("${kaptcha.textproducer.font.color}")
+    private String fcolor;
+
+    @Value("${kaptcha.image.width}")
+    private String width;
+
+    @Value("${kaptcha.textproducer.char.string}")
+    private String cString;
+
+    @Value("${kaptcha.image.height}")
+    private String height;
+
+    @Value("${kaptcha.textproducer.font.size}")
+    private String fsize;
+
+    @Value("${kaptcha.noise.color}")
+    private String nColor;
+
+    @Value("${kaptcha.textproducer.char.length}")
+    private String clength;
+
+    @Value("${kaptcha.textproducer.font.names}")
+    private String fnames;
+
+    /**
+     * 由于web.xml不生效了，需要在这里配置Kaptcha验证码Servlet
+     */
+    @Bean
+    public ServletRegistrationBean<KaptchaServlet> servletRegistrationBean() throws ServletException {
+        ServletRegistrationBean<KaptchaServlet> servlet = new ServletRegistrationBean<KaptchaServlet>(new KaptchaServlet(), "/Kaptcha");
+        servlet.addInitParameter("kaptcha.border", border);// 无边框
+        servlet.addInitParameter("kaptcha.textproducer.font.color", fcolor); // 字体颜色
+        servlet.addInitParameter("kaptcha.image.width", width);// 图片宽度
+        servlet.addInitParameter("kaptcha.textproducer.char.string", cString);// 使用哪些字符生成验证码
+        servlet.addInitParameter("kaptcha.image.height", height);// 图片高度
+        servlet.addInitParameter("kaptcha.textproducer.font.size", fsize);// 字体大小
+        servlet.addInitParameter("kaptcha.noise.color", nColor);// 干扰线的颜色
+        servlet.addInitParameter("kaptcha.textproducer.char.length", clength);// 字符个数
+        servlet.addInitParameter("kaptcha.textproducer.font.names", fnames);// 字体
+        return servlet;
+    }
 
 
 
