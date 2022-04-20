9938ee18dcffb0c92c088fc220d7bd11873269ee
isisisisisisithch <dallucus@gmail.com>
2022-04-20 14:50:31 -0400

    1.modified pathUtil

--- src/main/java/ca/bytetube/communityApp/util/PathUtil.java
+++ src/main/java/ca/bytetube/communityApp/util/PathUtil.java
@@ -1,23 +1,70 @@
 package ca.bytetube.communityApp.util;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class PathUtil {
-	private static String separator = System.getProperty("file.separator");
+	private static String seperator = System.getProperty("file.separator");
+
+	private static String winPath;
+
+	private static String linuxPath;
+
+	private static String shopPath;
+
+	private static String headLinePath;
+
+	private static String shopCategoryPath;
+
+	@Value("${win.base.path}")
+	public void setWinPath(String winPath) {
+		PathUtil.winPath = winPath;
+	}
+
+	@Value("${linux.base.path}")
+	public void setLinuxPath(String linuxPath) {
+		PathUtil.linuxPath = linuxPath;
+	}
+
+	@Value("${shop.relevant.path}")
+	public void setShopPath(String shopPath) {
+		PathUtil.shopPath = shopPath;
+	}
+
+	@Value("${headline.relevant.path}")
+	public void setHeadLinePath(String headLinePath) {
+		PathUtil.headLinePath = headLinePath;
+	}
+
+	@Value("${shopcategory.relevant.path}")
+	public void setShopCategoryPath(String shopCategoryPath) {
+		PathUtil.shopCategoryPath = shopCategoryPath;
+	}
 
 	public static String getImgBasePath() {
 		String os = System.getProperty("os.name");
 		String basePath = "";
 		if (os.toLowerCase().startsWith("win")) {
-			basePath = "D:/projectdev/image";
+			basePath = winPath;
 		} else {
-			basePath = "/Users/dalluo/Desktop/image";
+			basePath = linuxPath;
 		}
-		basePath = basePath.replace("/", separator);
+		basePath = basePath.replace("/", seperator);
 		return basePath;
 	}
 
 	public static String getShopImagePath(long shopId) {
-		String imagePath = "/upload/images/item/shop/" + shopId + "/";
-		return imagePath.replace("/", separator);
+		String imagePath = shopPath + shopId + seperator;
+		return imagePath.replace("/", seperator);
 	}
 
+	public static String getHeadLineImagePath() {
+		return headLinePath.replace("/", seperator);
+	}
+
+	public static String getShopCategoryPath() {
+		return shopCategoryPath.replace("/", seperator);
+	}
 }
+
